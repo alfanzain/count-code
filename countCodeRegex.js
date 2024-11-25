9994ef6729687e60acc1944e6e535ae79092f91e
Alfan Zain <alfanzainkp@gmail.com>
2024-11-25 13:29:09 +0700

    chore: separate test cases

--- countCodeRegex.js
+++ countCodeRegex.js
@@ -11,28 +11,4 @@ function countCode(str) {
     return matches?.length ?? 0;
 }
 
-console.log("Test cases:")
-countCode('aaacodebbb') // 1
-countCode('codexxcode') // 2
-countCode('cozexxcope') // 2
-
-console.log("Extra test cases:")
-countCode('abcdefg') // 0
-countCode('code') // 1
-countCode('core') // 1
-countCode('cores') // 1
-countCode('icore') // 1
-
-console.log("Possible negative cases:")
-countCode('') // 0
-countCode('a') // 0
-countCode('ab') // 0
-countCode('abc') // 0
-countCode('abcd') // 0
-countCode(undefined) // 0
-countCode(null) // 0
-countCode(1234124151234123) // 0
-countCode({name: "code"}) // 0
-countCode([]) // 0
-countCode(["code"]) // 0
-countCode(["c", "o", "d", "e"]) // 0
+module.exports = countCode;
